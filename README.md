c1cd55758e59955fedc1c3042943e136681ab4c6
ozaner <56015962+ozaner@users.noreply.github.com>
2024-10-29 18:52:50 -0400

    Create stream-tts example

--- README.md
+++ README.md
@@ -12,7 +12,16 @@ alltalk-api = { git = "https://github.com/ozaner/alltalk-api.git", tag = "2.0.0"
 
 The tag version corresponds to the version of AllTalk the crate matches.
 
-## API Version Compatibility
+### API Version Compatibility
 This crate is designed to match the v2 AllTalk API, which is still in progress on the [Beta branch](https://github.com/erew123/alltalk_tts/tree/alltalkbeta). As such, AllTalk may have breaking changes that make this crate no longer compatible. The current commit hash of the beta this crate has been tested with is [`0c9034c`](https://github.com/erew123/alltalk_tts/commit/0c9034c1f32a65573c3c4df4f9c907cd32b19be4).
 
 While it may work with newer versions, no guarantees are made.
+
+## Example
+To run an example program that plays streamed TTS audio straight from the terminal, run the following in the root directory of the repo:
+
+```
+cargo run --example stream-tts
+```
+
+This example assumes AllTalk is hosted on the default local endpoint, and that it supports streamed audio in the currently switched engine.
---
+++ examples/stream-tts.rs
@@ -0,0 +1,75 @@
+use alltalk_api::Client;
+use rodio::{OutputStream, Sink, Source};
+use std::io::{self, Write};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let mut input = String::new();
+
+    // Check if the server is ready before we start
+    match client.get_ready().await {
+        Ok(ready) => {
+            if ready {
+                println!("Server is ready.")
+            } else {
+                return Err("Server is not ready or unavailable.".into());
+            }
+        }
+        Err(err) => {
+            return Err(format!("Error checking server readiness: {err}").into());
+        }
+    }
+
+    // Try to turn deepspeed on
+    client.set_deepspeed(true).await?;
+
+    // Set up the audio output
+    let (_stream, stream_handle) = OutputStream::try_default()?;
+    let sink = Sink::try_new(&stream_handle)?;
+
+    // Choose voice
+    let voices = client.get_voices_list().await?;
+    println!("Available voices:");
+    for (i, voice) in voices.iter().enumerate() {
+        println!("  {}: {}", i, voice);
+    }
+    print!("Enter voice index: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut input)?;
+    let voice = &voices[match input.trim().parse::<usize>() {
+        Ok(i) if i < voices.len() => i,
+        _ => return Err("Invalid voice index.".into()),
+    }];
+    input.clear();
+
+    // Input loop
+    loop {
+        // Get input from the user
+        print!("Enter text to generate TTS (or type 'q' to quit): ");
+        io::stdout().flush()?;
+        io::stdin().read_line(&mut input)?;
+
+        let trimmed = input.trim().to_owned();
+
+        // Quit if the user types "q"
+        if trimmed == "q" {
+            break;
+        }
+
+        // Generate the TTS stream
+        match client.generate_tts_stream(trimmed, voice, "en").await {
+            Ok(streaming_source) => {
+                println!("Playing audio...");
+                sink.append(streaming_source.convert_samples::<f32>());
+                sink.sleep_until_end(); // Wait for audio to finish playing
+            }
+            Err(err) => eprintln!("Error generating TTS stream: {:?}", err),
+        }
+
+        input.clear();
+    }
+
+    println!("quitting...");
+    Ok(())
+}
